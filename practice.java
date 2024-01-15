bd0d49103e241b8b5dceace22e49e223895dc69d
임성현 <shlim0287@gmail.com>
2024-01-15 09:18:13 +0900

    feat : 1만시간의 법칙 과제물 추가

---
+++ practice.java
@@ -0,0 +1,11 @@
+public class practice {
+    public static void main(String[] args) {
+        int a= 10;
+        int b= a;
+        System.out.println(a);
+        System.out.println(b);
+        a=20;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
